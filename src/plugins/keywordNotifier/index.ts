4b180f7a7924c467a552c155d34b5d384546908c
Kewi <iamkewwie@gmail.com>
2025-01-15 16:18:25 +0100

    updated checks and added new setting to ignoreMentions

--- src/plugins/keywordNotifier/index.ts
+++ src/plugins/keywordNotifier/index.ts
@@ -11,13 +11,13 @@ import { Devs } from "@utils/constants";
 import { getCurrentChannel } from "@utils/discord";
 import definePlugin, { OptionType } from "@utils/types";
 import { ChannelStore, GuildStore, NavigationRouter, UserStore } from "@webpack/common";
-import { Message } from "discord-types/general";
+import { MessageJSON } from "discord-types/general";
 
 interface MessageContext {
     channelId: string;
     guildId: string;
     isPushNotification: boolean;
-    message: Message;
+    message: MessageJSON;
     optimistic: boolean;
     type: string;
 }
@@ -28,7 +28,6 @@ interface NotificationOptions {
         username: string;
         discriminator: string;
         avatarUrl: string;
-        bot: boolean;
     };
     keyword: string;
     guildId: string;
@@ -83,11 +82,16 @@ const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         description: "Ignore messages from bots",
         default: false
+    },
+    ignoreMentions: {
+        type: OptionType.BOOLEAN,
+        description: "Ignore messages that mention you",
+        default: true
     }
 });
 
-function getUserTag(username: string, discriminator: string, bot: boolean) {
-    return `${username}${bot ? `#${discriminator}` : ""}`;
+function getUserTag(username: string, discriminator: string) {
+    return `${username}${discriminator !== "0" ? `#${discriminator}` : ""}`;
 }
 
 function getUserAvatarUrl(userId: string, avatar: string) {
@@ -99,10 +103,11 @@ function Notify(options: NotificationOptions) {
 
     if (settings.store.notifications === "inApp" || settings.store.notifications === "both") {
         Notices.showNotice(
-            `@${getUserTag(author.username, author.discriminator, author.bot)} mentioned "${keyword}" in ${GuildStore.getGuild(guildId)?.name}`,
+            `@${getUserTag(author.username, author.discriminator)} mentioned "${keyword}" in ${GuildStore.getGuild(guildId)?.name}`,
             "Go To Message",
             () => {
                 NavigationRouter.transitionTo(`/channels/${guildId}/${channelId}/${message.id}`);
+                Notices.popNotice();
             }
         );
     }
@@ -122,8 +127,13 @@ function Notify(options: NotificationOptions) {
 function onMessageCreate(ctx: MessageContext) {
     if (!ctx.guildId) return;
     if (ctx.isPushNotification) return;
-    if (ctx.message.author.id === UserStore.getCurrentUser().id) return;
-    if (ctx.channelId === getCurrentChannel()?.id) return;
+
+    var currentUser = UserStore.getCurrentUser();
+    var currentChannel = getCurrentChannel();
+
+    if (settings.store.ignoreMentions && ctx.message.mentions.some(mention => mention.id === currentUser?.id)) return;
+    if (ctx.message.author.id === currentUser?.id) return;
+    if (ctx.channelId === currentChannel?.id) return;
 
     var allowedGuilds = settings.store.allowedGuilds.length > 0 ? settings.store.allowedGuilds.split(",").map(id => id.trim()) : [];
     var allowedChannels = settings.store.allowedChannels.length > 0 ? settings.store.allowedChannels.split(",").map(id => id.trim()) : [];
@@ -131,13 +141,13 @@ function onMessageCreate(ctx: MessageContext) {
     var ignoredChannels = settings.store.ignoredChannels.length > 0 ? settings.store.ignoredChannels.split(",").map(id => id.trim()) : [];
     var ignoredUsers = settings.store.ignoredUsers.length > 0 ? settings.store.ignoredUsers.split(",").map(id => id.trim()) : [];
 
-    if ((allowedGuilds.length > 0 && !allowedGuilds.includes(ctx.guildId)) && (allowedChannels.length > 0 && !allowedChannels.includes(ctx.channelId))) return console.log("not allowed");
+    if ((allowedGuilds.length > 0 && !allowedGuilds.includes(ctx.guildId)) && (allowedChannels.length > 0 && !allowedChannels.includes(ctx.channelId))) return;
 
-    if (ignoredGuilds.length > 0 && ignoredGuilds.includes(ctx.guildId)) return;
-    if (ignoredChannels.length > 0 && ignoredChannels.includes(ctx.channelId)) return;
-    if (ignoredUsers.length > 0 && ignoredUsers.includes(ctx.message.author.id)) return;
+    if (ignoredGuilds.includes(ctx.guildId)) return;
+    if (ignoredChannels.includes(ctx.channelId)) return;
+    if (ignoredUsers.includes(ctx.message.author.id)) return;
 
-    if (settings.store.ignoreBots && ctx.message.author.bot) return;
+    if (settings.store.ignoreBots && UserStore.getUser(ctx.message.author.id)?.bot) return;
 
     var keywords = settings.store.keywords.split(",").map(keyword => keyword.trim().toLowerCase());
     for (var keyword of keywords) {
@@ -147,8 +157,7 @@ function onMessageCreate(ctx: MessageContext) {
                     id: ctx.message.author.id,
                     username: ctx.message.author.username,
                     discriminator: ctx.message.author.discriminator,
-                    avatarUrl: getUserAvatarUrl(ctx.message.author.id, ctx.message.author.avatar),
-                    bot: ctx.message.author.bot
+                    avatarUrl: getUserAvatarUrl(ctx.message.author.id, ctx.message.author.avatar)
                 },
                 keyword,
                 guildId: ctx.guildId,
